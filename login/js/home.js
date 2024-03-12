ed6bf4e50722640908013fbcbb04a0d7e5b288de
Francisco David <fdavid04@icloud.com>
2024-03-11 21:09:21 -0600

    upload files

--- login/js/home.js
+++ login/js/home.js
@@ -4,4 +4,67 @@ let photo= document.getElementById("photo");
 photo.src= localStorage.getItem("photo");
 
 
+const sumaButton = document.getElementById("sum");
+const restaButton = document.getElementById("rest");
+const divisionButton = document.getElementById("div");
+const multiplicacionButton = document.getElementById("multi");
+const operacion = document.getElementById("operation");
+const form = document.getElementById("form");
+const resultado = document.getElementById("result");
 
+form.addEventListener("reset", function() {
+    resultado.textContent = "";
+    operacion.textContent = "";
+});
+
+sumaButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    
+    if (num1 === "" || num2 === "") {
+        resultado.textContent = "Campos vacíos";
+    } else {
+        resultado.textContent = parseInt(num1) + parseInt(num2);
+        operacion.textContent = "+";
+    }
+});
+
+restaButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    
+    if (num1 === "" || num2 === "") {
+        resultado.textContent = "Campos vacíos";
+    } else {
+        resultado.textContent = parseInt(num1) - parseInt(num2);
+        operacion.textContent = "-";
+    }
+});
+
+divisionButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    
+    if (num1 === "" || num2 === "") {
+        resultado.textContent = "Campos vacíos";
+    } else {
+        resultado.textContent = parseInt(num1) / parseInt(num2);
+        operacion.textContent = "/";
+    }
+});
+
+multiplicacionButton.addEventListener("click", function(e) {
+    e.preventDefault();
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    
+    if (num1 === "" || num2 === "") {
+        resultado.textContent = "Campos vacíos";
+    } else {
+        resultado.textContent = parseInt(num1) * parseInt(num2);
+        operacion.textContent = "*";
+    }
+});
