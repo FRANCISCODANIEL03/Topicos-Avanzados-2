1216047f8922081fb86483c7173bd723361bd2da
itsFDavid <fdavid04@icloud.com>
2024-06-11 07:40:31 -0600

    chore: Update socket.io dependency to version 4.7.5

--- nodeJs/Chat-Real-Time/server/index.js
+++ nodeJs/Chat-Real-Time/server/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { createServer } from 'http';
+import { Server, createServer} from 'node:http';
+
 
 const PORT = process.env.PORT || 3000;
 
 
 const app = express();
 app.use(cors());
+
 
 
 app.get('/', (req, res) => {
