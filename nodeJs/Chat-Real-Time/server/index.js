94e3b93953ae1f527edbe582bb69f00862869771
itsFDavid <fdavid04@icloud.com>
2024-06-10 21:42:16 -0600

    chore: Add server file for chat application

---
+++ nodeJs/Chat-Real-Time/server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { createServer } from 'http';
+
+const PORT = process.env.PORT || 3000;
+
+
+const app = express();
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/cliente/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listo en el puerto ${PORT}`);
+});
